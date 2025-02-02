b11e6a8637a92d31d2cf03e02e6f049e567845c3
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2025-02-02 19:50:26 +0100

    feat: formula for subtraction operations

--- js/operations.js
+++ js/operations.js
@@ -11,3 +11,16 @@ document.querySelector('form').addEventListener('submit', function(e){
     document.getElementById('result').textContent = `Risultato: ${inputone} + ${inputtwo} = ${risultato}`
     form.reset();
 })
+
+document.getElementById('subtraction').addEventListener('submit', function(e){
+    e.preventDefault();
+    let form = e.target; 
+    let inputthree = document.getElementById('inputthree').value;
+    let inputfour = document.getElementById('inputfour').value;
+    inputthree= parseInt(inputthree);
+    inputfour= parseInt(inputfour);
+    let risultatosub = inputthree - inputfour;
+    console.log(inputthree, inputfour, risultatosub);
+    document.getElementById('resulttwo').textContent = `Risultato: ${inputthree} - ${inputfour} = ${risultatosub}`
+    form.reset();
+})
